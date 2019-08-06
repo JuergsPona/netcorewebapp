93101eed45fcfb98aeaa6d7d5fec467d3933c0f3
JuergsPona <juergen@arium.co>
2019-08-06 14:52:51 +0200

    first commit

---
+++ Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyTree.Models
+{
+    public partial class Person
+    {
+        public int PersonId { get; set; }
+        public string FullName { get; set; }
+        public Status IsDeceased { get; set; }
+        public string Mother { get; set; }
+        public string Father { get; set; }
+        public short IsParent { get; set; }
+    }
+
+    public enum Status : short
+    {
+        Alive,
+        Dead
+    }
+}
---
+++ Models/familytreedbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace FamilyTree.Models
+{
+    public partial class familytreedbContext : DbContext
+    {
+        public familytreedbContext()
+        {
+        }
+
+        public familytreedbContext(DbContextOptions<familytreedbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=i-ken\\sqlexpress;Database=familytreedb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.HasKey(e => e.PersonId);
+
+                entity.Property(e => e.Father).HasMaxLength(100);
+
+                entity.Property(e => e.FullName).HasMaxLength(100);
+
+                entity.Property(e => e.Mother).HasMaxLength(100);
+            });
+        }
+    }
+}
